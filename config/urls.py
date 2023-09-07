44d055d1e75d6ed92a7df0dba878fb8b8de73bff
Tomilola-ng <tee.o2809@gmail.com>
2023-09-07 02:42:36 +0100

    HNGX BackendTrack Task1

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static  import static
 from api.views import get_info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_info, name="get_info"),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
